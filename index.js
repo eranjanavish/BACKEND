fe893e737cd10178cfb7a1ddc1efd8d0ff6849fb
eranjanavish <eranjanaamarasinghe@gmail.com>
2026-05-24 00:12:00 +0530

    port

--- index.js
+++ index.js
@@ -5,25 +5,25 @@ import PG from "pg"
 import 'dotenv/config'
 
 
-const port = 3000;
+const port = process.env.PORT || 3000;
+
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(cors());
 
-const db = new PG.Client({
-    user:"postgres",
-    host:"localhost",
-    database:"commit",
-    port:5432,
-    password:process.env.sqlpassword
-})
-
+const db = new PG.Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
+  });
 db.connect()
+
+
+  
 
 app.get("/",async (req,res)=>{
     const joke =  await getData(req.query.extype);
-    console.log(joke);
+    
     res.send(joke);
 })
 
@@ -32,25 +32,21 @@ app.listen(port,()=>{
     console.log("Server is runnig on ",port);
 })
 
-const getData = async(type)=>{
+const getData = async (type) => {
     console.log(type);
-    try{
-        let response;
-        if(type == "Any")
-            response = await db.query("select * from commit");
-        else{
-            response = await db.query("select * from commit where extype=$1",[type]);
-        }
-        
-        if(response.rows.length == 0){
-            return("No commit msg found")
-        }
-        
-        return  response.rows[Math.floor(Math.random()*response.rows.length)].text;
-        
+    try {
+      let response;
+      if (type == "Any")
+        response = await db.query(`SELECT * FROM public.commits`);
+      else
+        response = await db.query(`SELECT * FROM public.commits WHERE extype=$1`, [type]);
+  
+      if (response.rows.length == 0) return "No commit msg found";
+  
+      return response.rows[Math.floor(Math.random() * response.rows.length)].text;
+  
+    } catch (err) {
+      console.log("Error when connecting to database ||", err);
+      return "Database error";
     }
-    catch(err){
-        console.log("Error when connecting to database ||",err);
-        return("Data base error");
-    }
-}
+  };
